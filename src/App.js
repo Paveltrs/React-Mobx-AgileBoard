81738d72341b4ed690d8399d0968c69da2df2753
Pavel <84628503+Paveltrs@users.noreply.github.com>
2021-09-30 22:07:25 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return (
+  <>
+    <Header />
+    <main>
+      <Dashboard />
+    </main>
+  </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React, {createContext} from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import RootStore from './store'
+import {CssBaseline} from '@material-ui/core'
+
+const store = RootStore.create({});
+
+export const StoreContext = createContext(store)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StoreContext.Provider value={store}>
+      <CssBaseline />
+    <App />
+    </StoreContext.Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
